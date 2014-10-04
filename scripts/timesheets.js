a2084194d1354ca7909b33ffd10526fc2d1371e5
Yuichiro MASUI <masui@masuidrive.jp>
2014-10-04 23:42:53 +0900

    順調にテスト

--- scripts/timesheets.js
+++ scripts/timesheets.js
@@ -24,14 +24,23 @@ loadTimesheets = function (exports) {
     var ignore_users = String(this.settings.get('無視するユーザ')).toLowerCase().trim().split(/\s*,\s*/);
     if(_.contains(ignore_users, username.toLowerCase())) return;
 
+    // 日付は先に処理しておく
+    this.date = DateUtils.parseDate(message);
+    this.time = DateUtils.parseTime(message);
+    this.datetime = DateUtils.normalizeDateTime(this.date, this.time);
+    if(this.datetime !== null) {
+      this.dateStr = DateUtils.format("Y/m/d", this.datetime);
+      this.datetimeStr = DateUtils.format("Y/m/d H:i", this.datetime);
+    }
+
     // コマンド集
     var commands = [
-      ['actionOut', /(バ[ー〜ァ]*イ|ば[ー〜ぁ]*い|おやすみ|お[つっ]|お先|お疲|帰|乙|bye|night|(c|see)\s*(u|you)|退勤|ごきげんよう|グッバイ|ばい)/],
+      ['actionSignOut', /(バ[ー〜ァ]*イ|ば[ー〜ぁ]*い|おやすみ|お[つっ]|お先|お疲|帰|乙|bye|night|(c|see)\s*(u|you)|退勤|ごきげんよう|グッバイ|ばい)/],
       ['actionWhoIsOff', /(だれ|誰|who\s*is).*(休|やす(ま|み|む))/],
       ['actionWhoIsIn', /(だれ|誰|who\s*is)/],
       ['actionCancelOff', /(休|やす(ま|み|む)).*(キャンセル|消|止|やめ|ません)/],
       ['actionOff', /(休|やす(ま|み|む))/],
-      ['actionIn', /(モ[ー〜]+ニン|も[ー〜]+にん|おっは|おは|へろ|はろ|ヘロ|ハロ|hi|hello|morning|出勤)/],
+      ['actionSignIn', /(モ[ー〜]+ニン|も[ー〜]+にん|おっは|おは|へろ|はろ|ヘロ|ハロ|hi|hello|morning|出勤)/],
     ];
 
     // メッセージを元にメソッドを探す
@@ -46,58 +55,61 @@ loadTimesheets = function (exports) {
   }
 
   // 出勤
-  exports.Timesheets.prototype.actionIn = function(username, message) {
-    var date = DateUtils.parseDate(message);
-    var time = DateUtils.parseTime(message);
-    var datetime = DateUtils.normalizeDateTime(date, time);
-    if(datetime) {
-      var result = this.storage.doIn(username, datetime, !!time, message);
-      if(result == 'ok') {
-        this.responder.template("出勤", username, DateUtils.format("Y/m/d H:i", datetime));
+  exports.Timesheets.prototype.actionSignIn = function(username, message) {
+    if(this.datetime) {
+      var data = this.storage.get(username, this.datetime);
+      if(typeof data.signIn == 'undefined' || data.signIn === '-') {
+        this.storage.set(username, this.datetime, {signIn: this.datetime});
+        this.responder.template("出勤", username, this.datetimeStr);
       }
-      else if(result == 'updated') {
-        this.responder.template("出勤更新", username, Utilities.formatDate(datetime, Session.getScriptTimeZone(), "HH:mm"));
+      else {
+        // 更新の場合は時間を明示する必要がある
+        if(!!this.time) {
+          this.storage.set(username, this.datetime, {signIn: this.datetime});
+          this.responder.template("出勤更新", username, this.datetimeStr);
+        }
       }
-    }
-    else if(date) {
-      this.responder.template("休暇取消", username, Utilities.formatString("%02d/%02d", date[1], date[2]));
     }
   };
 
   // 退勤
-  exports.Timesheets.prototype.actionOut = function(username, message) {
-    var date = parseDate(message);
-    var time = parseTime(message);
-    var datetime = normalizeDateTime(date, time);
-    if(datetime) {
-      var result = this.storage.doOut(username, datetime, !!time, message);
-      if(result == 'ok') {
-        this.responder.template("退勤", username, Utilities.formatDate(datetime, Session.getScriptTimeZone(), "yyyy/MM/dd HH:mm"));
+  exports.Timesheets.prototype.actionSignOut = function(username, message) {
+    if(this.datetime) {
+      var data = this.storage.get(username, this.datetime);
+      if(typeof data.signOut == 'undefined' || data.signOut === '-') {
+        this.storage.set(username, this.datetime, {signOut: this.datetime});
+        this.responder.template("退勤", username, this.datetimeStr);
       }
-      else if(result == 'updated') {
-        this.responder.template("退勤更新", username, Utilities.formatDate(datetime, Session.getScriptTimeZone(), "HH:mm"));
+      else {
+        // 更新の場合は時間を明示する必要がある
+        if(!!this.time) {
+          this.storage.set(username, this.datetime, {signOut: this.datetime});
+          this.responder.template("退勤更新", username, this.datetimeStr);
+        }
       }
     }
   };
 
   // 休暇申請
   exports.Timesheets.prototype.actionOff = function(username, message) {
-    var date = parseDate(message);
-    if(date) {
-      var result = this.storage.doOff(username, normalizeDateTime(date, [0,0]), message);
-      if(result) {
-        this.responder.template("休暇", username, Utilities.formatString("%02d/%02d", date[1], date[2]));
+    if(this.date) {
+      var dateObj = new Date(this.date[0], this.date[1]-1, this.date[2]);
+      var data = this.storage.get(username, dateObj);
+      if(typeof data.signOut == 'undefined' || data.signOut === '-') {
+        this.storage.set(username, dateObj, {signIn: '-', signOut: '-', note: message});
+        this.responder.template("休暇", username, DateUtils.format("Y/m/d", dateObj));
       }
     }
   };
 
   // 休暇取消
   exports.Timesheets.prototype.actionCancelOff = function(username, message) {
-    var date = parseDate(message);
-    if(date) {
-      var result = this.storage.doCancelOff(username, normalizeDateTime(date, [0,0]), message);
-      if(result) {
-        this.responder.template("休暇取消", username, Utilities.formatString("%02d/%02d", date[1], date[2]));
+    if(this.date) {
+      var dateObj = new Date(this.date[0], this.date[1]-1, this.date[2]);
+      var data = this.storage.get(username, dateObj);
+      if(typeof data.signOut == 'undefined' || data.signOut === '-') {
+        this.storage.set(username, dateObj, {signIn: null, signOut: null, note: message});
+        this.responder.template("休暇取消", username, DateUtils.format("Y/m/d", dateObj));
       }
     }
   };
--- tests/timesheets_test.js
+++ tests/timesheets_test.js
@@ -15,9 +15,35 @@ QUnit.test( "EventListener", function(assert) {
   };
 
   var storage = {
-    doIn: function() {
-      /* noop */
-      return 'ok';
+    data: {},
+
+    init: function(initData) {
+      this.data = _.clone(initData || {});
+    },
+
+    get: function(username, date) {
+      if(!this.data[username]) this.data[username] = {};
+      return this.data[username][String(DateUtils.toDate(date))] || {};
+    },
+
+    set: function(username, date, params) {
+      var row = this.get(username, date);
+
+      if(typeof params.signIn != 'undefined') {
+        row.signIn = params.signIn;
+      }
+
+      if(typeof params.signOut != 'undefined') {
+        row.signOut = params.signOut;
+      }
+
+      if(typeof params.note != 'undefined') {
+        row.note = params.note;
+      }
+
+      this.data[username][String(DateUtils.toDate(date))] = row;
+
+      return row;
     }
   };
 
@@ -31,31 +57,88 @@ QUnit.test( "EventListener", function(assert) {
     }
   };
 
+
+  var msgTest = function(user, msg, result) {
+    responder.clearMessages();
+    timesheets.receiveMessage(user, msg);
+    assert.ok(_.isEqual(result, responder.messages), user+":"+msg);
+  };
+
+  var storageTest = function(initData, callback) {
+    callback(function(user, msg, result) {
+      storage.init(initData);
+      msgTest(user, msg, result);
+    });
+  };
+
+
   var timesheets = new Timesheets(storage, settings, responder);
 
   DateUtils.now(new Date(2014,0,2,12,34,0));
+  var nowDateStr = String(new Date(2014,0,2));
 
-  responder.clearMessages();
-  timesheets.receiveMessage('test1', 'おはよう');
-  assert.ok(_.isEqual([['出勤', 'test1', "2014/01/02 12:34"]], responder.messages), 'おはよう');
+  // 出勤
+  storageTest({}, function(msgTest) {
+    msgTest('test1', 'おはよう', [['出勤', 'test1', "2014/01/02 12:34"]]);
+    msgTest('test1', 'おはよう 4:56', [['出勤', 'test1', "2014/01/02 04:56"]]);
+    msgTest('test1', 'おはよう 4:56 2/3', [['出勤', 'test1', "2014/02/03 04:56"]]);
+  });
+
+  // 出勤時間の変更
+  var test1 = {};
+  test1[nowDateStr] = { signIn: new Date(2014,0,2,0,0,0) };
+  storageTest({'test1': test1}, function(msgTest) {
+    msgTest('test1', 'おはよう', []);
+    msgTest('test1', 'おはよう 4:56', [['出勤更新', 'test1', "2014/01/02 04:56"]]);
+  });
+
+  // 退勤
+  storageTest({}, function(msgTest) {
+    msgTest('test1', 'おつ', [['退勤', 'test1', "2014/01/02 12:34"]]);
+    msgTest('test1', 'お疲れさま 14:56', [['退勤', 'test1', "2014/01/02 14:56"]]);
+    msgTest('test1', 'お疲れさま 16:23 12/3', [['退勤', 'test1', "2013/12/03 16:23"]]);
+  });
+
+  // 退勤時間の変更
+  var test1 = {};
+  test1[nowDateStr] = { signIn: new Date(2014,0,2,0,0,0), signOut: new Date(2014,0,2,12,0,0) };
+  storageTest({'test1': test1}, function(msgTest) {
+    msgTest('test1', 'おつ', []);
+    msgTest('test1', 'お疲れさま 14:56', [['退勤更新', 'test1', "2014/01/02 14:56"]]);
+  });
+
+  // 退勤時間の変更
+  var test1 = {};
+  test1[nowDateStr] = { signIn: new Date(2014,0,2,0,0,0), signOut: new Date(2014,0,2,12,0,0) };
+  storageTest({'test1': test1}, function(msgTest) {
+    msgTest('test1', 'おつ', []);
+    msgTest('test1', 'お疲れさま 14:56', [['退勤更新', 'test1', "2014/01/02 14:56"]]);
+  });
+
+  // 休暇申請
+  storageTest({}, function(msgTest) {
+    msgTest('test1', 'お休み', []);
+    msgTest('test1', '今日はお休み', [['休暇', 'test1', "2014/01/02"]]);
+    msgTest('test1', '明日はお休み', [['休暇', 'test1', "2014/01/03"]]);
+    msgTest('test1', '12/3はお休みでした', [['休暇', 'test1', "2013/12/03"]]);
+  });
+
+  // 休暇取消
+  var test1 = {};
+  test1[nowDateStr] = { signIn: '-', singOut: '-' };
+  storageTest({'test1': test1}, function(msgTest) {
+    msgTest('test1', 'お休みしません', []);
+    msgTest('test1', '今日はお休みしません', [['休暇取消', 'test1', "2014/01/02"]]);
+    msgTest('test1', '明日はお休みしません', [['休暇取消', 'test1', "2014/01/03"]]);
+  });
+
+
 /*
-  timesheets.receiveMessage('test1', 'おはよう');
-  timesheets.receiveMessage('test1', 'おつ');
-  timesheets.receiveMessage('test1', 'おつ 14:00');
-  timesheets.receiveMessage('test1', '明日はお休みです');
-  timesheets.receiveMessage('test1', '明日のお休みを取り消します');
-  timesheets.receiveMessage('test1', '明日はやっぱり出勤します');
-  timesheets.receiveMessage('test1', '10/1はお休みです');
-
   timesheets.receiveMessage('test1', '誰がいる？');
   timesheets.receiveMessage('test1', '誰がお休み？');
   timesheets.receiveMessage('test1', '9/21 誰がお休み？');
   timesheets.receiveMessage('test1', '9/22 誰がお休み？');
   timesheets.receiveMessage('test1', '9/23 誰がお休み？');
-
-
-  timesheets.receiveMessage('test1', 'おはよう 10/2 10:00');
-  timesheets.receiveMessage('test1', 'おつ 10/3 4:00');
 
   // 無視される
   timesheets.receiveMessage('Slackbot', 'おはよう 10/1 10:00');
