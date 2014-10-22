59c7b10a99e059a22906ad75421e5dec5f430b37
Yuichiro MASUI <masui@masuidrive.jp>
2014-10-22 23:41:21 +0900

    アップデートチェックの間違えを修正

--- scripts/gas_utils.js
+++ scripts/gas_utils.js
@@ -13,13 +13,18 @@ checkUpdate = function(responder) {
   if(typeof GASProperties === 'undefined') GASProperties = loadGASProperties();
   var current_version = parseFloat(new GASProperties().get('version')) || 0;
 
-  // var response = UrlFetchApp.fetch("https://raw.githubusercontent.com/masuidrive/miyamoto/master/VERSION", {muteHttpExceptions: true});
-  var response = UrlFetchApp.fetch("https://raw.githubusercontent.com/masuidrive/miyamoto/feature/check_update/VERSION", {muteHttpExceptions: true});
+  var response = UrlFetchApp.fetch("https://raw.githubusercontent.com/masuidrive/miyamoto/master/VERSION", {muteHttpExceptions: true});
 
   if(response.getResponseCode() == 200) {
     var latest_version = parseFloat(response.getContentText());
     if(latest_version > 0 && latest_version > current_version) {
-      responder.send("最新版がリリースされています。 https://github.com/masuidrive/miyamoto/blob/master/UPDATE.md を実行してください。");
+      responder.send("最新版がリリースされています。\nhttps://github.com/masuidrive/miyamoto/blob/master/UPDATE.md を実行してください。");
+
+      var response = UrlFetchApp.fetch("https://raw.githubusercontent.com/masuidrive/miyamoto/master/HISTORY.md", {muteHttpExceptions: true});
+      if(response.getResponseCode() == 200) {
+        var text = String(response.getContentText()).replace(new RegExp("## "+current_version+"[\\s\\S]*", "m"), '');
+        responder.send(text);
+      }
     }
   }
 };
