2d3139a370500b52f704a46d6df294539c5d056b
toshikish <mo491ki730@gmail.com>
2018-08-07 16:54:37 +0900

    Install gulp and babel

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+
+gulp.task('compile', () => {
+  gulp.src('./scripts/*.js')
+    .pipe(babel())
+    .pipe(concat('main.gs'))
+    .pipe(gulp.dest('.'));
+});
